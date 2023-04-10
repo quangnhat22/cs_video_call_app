b560c5aa6088ee291dc4f59e96cf488e01ae4f68
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-04-10 11:09:31 +0700

    handle navigation and add Sign Up page UI

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -15,3 +15,11 @@ class _Config {
     BASE_URL: 'https://api-dev',
   };
 }
+
+extension Validator on String {
+  bool isValidEmail() {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(this);
+  }
+}
---
+++ lib/presentation/auth/forgot_password/view/success_notification_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:videocall/presentation/auth/login/view/login_page.dart';
+
+class SuccessNotificationPage extends StatelessWidget {
+  const SuccessNotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        fit: StackFit.expand,
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 30, left: 12, right: 12),
+            decoration: const BoxDecoration(
+              color: Color.fromRGBO(52, 168, 83, 1),
+            ),
+            child: Column(children: [
+              SizedBox(
+                width: 170,
+                height: 170,
+                child: Image.asset('assets/images/success.png'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 40),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                child: const Text(
+                  'Awesome!!',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(52, 168, 83, 1)),
+                ),
+              )
+            ]),
+          ),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                child: Column(children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 40),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: const Text(
+                      'You have successfully reset password',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(52, 168, 83, 1)),
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.symmetric(
+                                    vertical: 14, horizontal: 20)),
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color.fromRGBO(0, 108, 69, 1)),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12)))),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginPage(),
+                              ));
+                        },
+                        child: const Text('BACK TO LOGIN')),
+                  )
+                ]),
+              ))
+        ],
+      )),
+    );
+  }
+}
