eee97e1e14d4e7b5755dc192c393325689f0af0f
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2023-05-02 10:50:28 +0700

    update some importings

--- lib/presentation/friends/friends_dash_board/friends_dash_board.dart
+++ lib/presentation/friends/friends_dash_board/friends_dash_board.dart
@@ -3,10 +3,10 @@ library friends_dash_board;
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:videocall/common/widgets/stateless/app_bar/m_app_bar.dart';
+import 'package:videocall/common/widgets/stateless/divider/divider_space_left.dart';
 
 import '../../../core/routes/app_navigation.dart';
 import '../../../core/routes/route_name.dart';
-import '../../../core/utils/screen_utils.dart';
 
 part 'pages/friends_dash_board_page.dart';
 part 'widgets/dropdown_filter_button.dart';
